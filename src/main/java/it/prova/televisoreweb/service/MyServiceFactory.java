c16c1af03ae3ae58e12b64aea6f97a7758f403d0
AlessioConti <alessioconti02@yahoo.it>
2022-05-27 22:52:08 +0200

    Implementazione MyServiceFactory

--- src/main/java/it/prova/televisoreweb/service/MyServiceFactory.java
+++ src/main/java/it/prova/televisoreweb/service/MyServiceFactory.java
@@ -1,5 +1,18 @@
 package it.prova.televisoreweb.service;
 
-public class MyServiceFactory {
+import it.prova.televisoreweb.dao.MyDaoFactory;
+import it.prova.televisoreweb.service.televisore.TelevisoreService;
+import it.prova.televisoreweb.service.televisore.TelevisoreServiceImpl;
 
+public class MyServiceFactory {
+	private static TelevisoreService TELEVISORE_SERVICE_INSTANCE;
+	
+	public static TelevisoreService getTelevisoreService() {
+		if(TELEVISORE_SERVICE_INSTANCE == null)
+			TELEVISORE_SERVICE_INSTANCE = new TelevisoreServiceImpl();
+		
+		TELEVISORE_SERVICE_INSTANCE.setTelevisoreDAO(MyDaoFactory.getTelevisoreDAOInstance());
+		
+		return TELEVISORE_SERVICE_INSTANCE;
+	}
 }
