ebd65013f8edb4d114f218a56619af982fb00387
AlessioConti <alessioconti02@yahoo.it>
2022-05-27 22:31:39 +0200

    Specializzazione TelevisoreDAO + Impl

--- src/main/java/it/prova/televisoreweb/dao/televisore/TelevisoreDAO.java
+++ src/main/java/it/prova/televisoreweb/dao/televisore/TelevisoreDAO.java
@@ -1,8 +1,10 @@
 package it.prova.televisoreweb.dao.televisore;
 
+import java.util.List;
+
 import it.prova.televisoreweb.dao.IBaseDAO;
 import it.prova.televisoreweb.model.Televisore;
 
 public interface TelevisoreDAO extends IBaseDAO<Televisore> {
-
+	public List<Televisore> findByMarcaEModello(String marca, String modello);
 }
--- src/main/java/it/prova/televisoreweb/dao/televisore/TelevisoreDAOImpl.java
+++ src/main/java/it/prova/televisoreweb/dao/televisore/TelevisoreDAOImpl.java
@@ -1,45 +1,68 @@
 package it.prova.televisoreweb.dao.televisore;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
+import it.prova.televisoreweb.dao.DB_Mock;
 import it.prova.televisoreweb.model.Televisore;
 
 public class TelevisoreDAOImpl implements TelevisoreDAO {
 
-	@Override
+	private Connection connection;
+	
+	public List<Televisore> findByMarcaEModello(String marca, String modello){
+		List<Televisore> risultati = new ArrayList<Televisore>();
+		
+		for(Televisore televisoreItem : DB_Mock.LISTA_TELEVISORI) {
+			if(televisoreItem.getMarca().startsWith(marca) && televisoreItem.getModello().startsWith(modello))
+				risultati.add(televisoreItem);
+		}
+		return risultati;
+	}
 	public List<Televisore> list() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return DB_Mock.LISTA_TELEVISORI;
 	}
 
 	@Override
 	public Televisore get(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Televisore temp = new Televisore();
+		for(Televisore televisoreItem : DB_Mock.LISTA_TELEVISORI) {
+			if(televisoreItem.getId().equals(id))
+				temp = televisoreItem;
+		}
+		return temp;
 	}
 
 	@Override
 	public int update(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
+		for(Televisore televisoreItem : DB_Mock.LISTA_TELEVISORI) {
+			if(televisoreItem.getId() == input.getId()) {
+				televisoreItem.setMarca(input.getMarca());
+				televisoreItem.setModello(input.getModello());
+				televisoreItem.setPrezzo(input.getPrezzo());
+				televisoreItem.setNumeroPollici(input.getNumeroPollici());
+				televisoreItem.setCodice(input.getCodice());
+				return 1;
+			}
+		}
 		return 0;
 	}
 
 	@Override
 	public int insert(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		input.setId(DB_Mock.getNexIdAvailable());
+		return DB_Mock.LISTA_TELEVISORI.add(input)?1:0;
 	}
 
 	@Override
 	public int delete(Televisore input) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		return DB_Mock.LISTA_TELEVISORI.remove(input)?1:0;
 	}
 
 	@Override
 	public void setConnection(Connection connection) {
-		// TODO Auto-generated method stub
+		this.connection = connection;
 
 	}
 
